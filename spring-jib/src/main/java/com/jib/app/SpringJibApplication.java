cd995e5926b71b048453a49c795e105189d09982
Mateus <mmachado.csp@gmail.com>
2018-09-10 22:46:30 -0300

    fix

--- spring-jib/src/main/java/com/jib/app/SpringJibApplication.java
+++ spring-jib/src/main/java/com/jib/app/SpringJibApplication.java
@@ -1,12 +1,20 @@
-package com.jib.springjib;
+package com.jib.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJibApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJibApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String up() {
+		return "up";
 	}
 }
